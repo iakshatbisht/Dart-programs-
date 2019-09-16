d043f96e7e34582ae31ed6d4346afdfce9470591
akshat102 <48102705+akshat102@users.noreply.github.com>
2019-09-16 21:52:25 +0530

    Update alertbox.dart.dart

--- alertbox.dart.dart
+++ alertbox.dart.dart
@@ -1,24 +1,33 @@
 import 'package:flutter/material.dart';
-
+//MaterialApp creates a space in your device to hold scaffold that contains all the widget
 void main() {
   runApp(new MaterialApp(home: new Application()));
 }
 
+//Stateful Widget is a widget that is not static. It can be scrollable, dynamic. It 
+//Stateful Widget is a widget that is not static. It can be scrollable, dynamic.
+//It implements the basic material design visual layout structure
 class Application extends StatefulWidget {
   @override
   _ApplicationState createState() => new _ApplicationState();
 }
 
 class _ApplicationState extends State<Application> {
-
+//mnethod to display the dialog box.
   void dialog(){
     showDialog(
         context: context,
         child: new AlertDialog(
+          //shows AlertDialog box
           title: new Text('Warning'),
+          //provide a title in your Alert bix
           content: new Text('You will die if you press on close button'),
+          //content to be displayed below title in your Alert dialog box
           actions: <Widget>[
-            new IconButton(icon: new Icon(Icons.close), onPressed: (){Navigator.pop(context);})
+            new IconButton(icon: new Icon(Icons.close), onPressed: (){
+              Navigator.pop(context);//it is used for routing. it closes the current paage and brings you back to the subsequent previous page
+            })
+          //provides an icon of close that performs the functionality of closing
           ],
         )
     );
@@ -28,8 +37,12 @@ class _ApplicationState extends State<Application> {
     return new Scaffold(
       body: new Center(
         child: new RaisedButton(
-            onPressed: (){dialog();},
+          //an elevated button
+            onPressed: (){dialog();
+                         //on clicking the button, the Alert dialog box appears as the method dialog() is invoked
+                         },
           child: new Text('Activate AlertDialog'),
+          //provides title to your raised button
         ),
       ) ,
     );
